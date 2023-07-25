d1057656e806dcf2bfc0998960ad7a284a9f9a4a
Julianlpz69 <julianjoselpz2019@gmail.com>
2023-07-25 17:24:24 -0500

    ss

---
+++ Actividad-Dicc/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Program {
+    
+
+    static Dictionary<string,Usuario> usuarios = new Dictionary<string,Usuario>();
+
+
+    class Usuario
+{
+    // Propiedades de la clase Usuario
+    public string Nombre { get; set; }
+    public string Edad { get; set; }
+    public List<string> Hobbies { get; set; }
+
+    // Constructor de la clase Usuario
+    public Usuario(string nombre, string edad, List<string> hobbies)
+    {
+        Nombre = nombre;
+        Edad = edad;
+        Hobbies = hobbies;
+    }
+}
+
+
+
+    static void Main() {
+
+
+        int opcion;
+
+        do {
+            MostrarMenu();
+            opcion = PedirOpcion();
+
+            switch (opcion) {
+                case 1:
+                    agregarUsuario();
+                    break;
+                case 2:
+                mostrarUsuario()
+
+                    ;
+                    break;
+                case 3:
+                mostrarUsuarios();
+                break;
+                case 4:
+                eliminarUsario();
+                    break;
+                default:
+                    Console.WriteLine("/nOpción inválida. Inténtalo de nuevo.");
+                    break;
+       }} while (opcion != 5);
+    }
+
+    static void MostrarMenu() {
+        Console.WriteLine("\nMenú de opciones:");
+        Console.WriteLine("\n1. Agregar Usuario");
+        Console.WriteLine("2. Mostrar Usuario");
+        Console.WriteLine("3. Mostrar Todos los Usuarios");
+        Console.WriteLine("4. Eliminar Usuario");
+        Console.WriteLine("5. Salir");
+    }
+
+    static int PedirOpcion() {
+        Console.Write("\nElige una opción: ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+
+
+    static void agregarUsuario(){
+
+        Console.Write("\nIngresa el numero de identificacion del usuario: ");
+        string? numero = Console.ReadLine();
+        Console.Write("\nIngresa el nombre del usuario: ");
+        string? nombre = Console.ReadLine();
+        Console.Write("\nIngresa la edad del usuario: ");
+        string? edad = Console.ReadLine();
+
+        List<string> hobbies = new List<string>();
+        string? hobby;
+         do{
+            Console.Write("Ingresa un hobbie (o escribe 'fin' para terminar): ");
+            hobby = Console.ReadLine();
+
+            if (hobby != "fin")
+            {
+                hobbies.Add(hobby);
+            }
+} while (hobby.ToLower() != "fin");
+
+        Usuario usuario = new Usuario(nombre,edad,hobbies);
+
+
+        Program.usuarios.Add($"{numero}", usuario);
+
+    }
+
+
+    static void mostrarUsuarios(){
+
+
+
+        Console.WriteLine($"\nIdentificacion------Nombre-------------Edad-----------");
+        foreach (var usuario in usuarios)
+        {
+            Console.WriteLine($"\n-----{usuario.Key}---------{usuario.Value.Nombre}-------------{usuario.Value.Edad}---");
+            Console.WriteLine($"\n-----Hobbies---------");
+
+            foreach (string hobby in usuario.Value.Hobbies)
+        {
+            Console.WriteLine($"- {hobby}");
+        }
+
+
+        }
+}
+
+
+static void mostrarUsuario(){
+
+
+
+        Console.Write("Ingresa el número de identificación del usuario: ");
+        string? numeroUsuario = Console.ReadLine();
+
+        // Verificar si el usuario existe en el diccionario
+        if (usuarios.TryGetValue(numeroUsuario, out Usuario usuarioEncontrado))
+        {
+            // Imprimir los datos del usuario encontrado
+            Console.WriteLine($"Datos del usuario con número de identificación {numeroUsuario}:");
+            Console.WriteLine($"Nombre: {usuarioEncontrado.Nombre}");
+            Console.WriteLine($"Edad: {usuarioEncontrado.Edad}");
+            Console.WriteLine("Hobbies:");
+            foreach (string hobby in usuarioEncontrado.Hobbies)
+            {
+                Console.WriteLine($"- {hobby}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Usuario con número de identificación {numeroUsuario} no encontrado.");
+        }
+
+}
+
+
+static void eliminarUsario(){
+
+
+
+        Console.Write("Ingresa el número de identificación del usuario: ");
+        string? numeroUsuario = Console.ReadLine();
+
+        // Verificar si el usuario existe en el diccionario
+        if (usuarios.TryGetValue(numeroUsuario, out Usuario usuarioEncontrado))
+        {
+          usuarios.Remove($"{numeroUsuario}");
+        }
+        else
+        {
+            Console.WriteLine($"Usuario con número de identificación {numeroUsuario} no encontrado.");
+        }
+
+}
+
+}
---
+++ dicc-Objetos/Program.cs
@@ -0,0 +1,53 @@
+﻿// Declaración e inicialización de un diccionario:
+
+using System;
+using System.Collections.Generic;
+
+class Program {
+    static void Main() {
+        // Declarar e inicializar un diccionario con clave de tipo string y valor de tipo int
+        Dictionary<string, int> miDiccionario = new Dictionary<string, int>();
+
+        // También puedes inicializar el diccionario con valores iniciales
+        Dictionary<string, string> paises = new Dictionary<string, string>
+        {
+            { "MX", "México" },
+            { "US", "Estados Unidos" },
+            { "CA", "Canadá" }
+        };
+    }
+}
+
+
+
+
+class Program {
+    static void Main() {
+    Dictionary<string, int> miDiccionario = new Dictionary<string, int>();
+
+
+    // Agregar elementos al diccionario:
+    miDiccionario.Add("uno", 1);
+    miDiccionario["dos"] = 2;
+
+    // Acceder y modificar elementos del diccionario:
+    int valorUno = miDiccionario["uno"];
+    Console.WriteLine("Valor de 'uno': " + valorUno); // Salida: Valor de 'uno': 1
+
+
+    // Verificar si una clave existe en el diccionario:
+    if (miDiccionario.ContainsKey("uno")) {
+    // La clave "uno" existe en el diccionario
+    }
+
+    // Recorrer el diccionario:
+    foreach (var clave in miDiccionario.Keys) {
+    Console.WriteLine("Clave: " + clave + ", Valor: " + miDiccionario[clave]);
+    }
+
+    // Eliminar elementos del diccionario:
+    miDiccionario.Remove("dos");
+    }
+}
+
+
